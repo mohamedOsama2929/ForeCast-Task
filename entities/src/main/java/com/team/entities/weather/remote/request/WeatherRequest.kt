a40bc41497015eb629939eb5031c5925e3340439
monk1996 <monkosama1996>
2021-08-27 17:34:39 +0200

    call data generally

--- entities/src/main/java/com/team/entities/weather/remote/request/WeatherRequest.kt
+++ entities/src/main/java/com/team/entities/weather/remote/request/WeatherRequest.kt
@@ -2,5 +2,5 @@ package com.team.entities.weather.remote.request
 
 data class WeatherRequest(
     val city: String? = null,
-    val apiKey: String = "33faf1e1239d19d363c9d897d98e0443"
+    val apiKey: String = "54869e72895250128aa6cddb65ea2f9a"
 )
